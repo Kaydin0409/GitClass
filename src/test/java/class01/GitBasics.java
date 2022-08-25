9539e7595b639286aca439dbda8f760f463c87f9
Kaydin0409 <klbond88@gmail.com>
2022-08-24 21:04:27 -0400

    2nd commit

--- src/test/java/class01/GitBasics.java
+++ src/test/java/class01/GitBasics.java
@@ -5,7 +5,7 @@ public class GitBasics {
     public static void main(String[] args) {
 
         //write down a code taht checks the temperature from a website and then prints out if it's a beautiful day or notls
-        
-        System.out.println("Today is a beautiful day");
+
+        System.out.println("Today is not a beautiful day");
     }
 }
